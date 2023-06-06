66dfacb4734e85d0525c62f9bdb4393a6e62f0bc
Ray <18924127468@163.com>
2023-06-07 02:19:47 +0800

    feat: new ar ui

--- components/ar-frame/index.js
+++ components/ar-frame/index.js
@@ -1,121 +1,113 @@
 function randomPosition() {
-  const paperX = 0.75,
-    paperZ = 0.75;
-  let paperCount = 10;
-  let position = [];
-  for (let i = 0; i < paperCount; i++) {
-    let x, y, z;
-    let ok = false;
-    while (!ok) {
-      ok = true;
-      x = Math.random() * 4 - 2;
-      y = Math.random() * 1;
-      z = Math.random() * 4 - 2;
-      for (let j = 0; j < i; j++) {
-        if (
-          Math.abs(x - position[j].x) < paperX &&
-          Math.abs(z - position[j].z) < paperZ
-        ) {
-          ok = false;
+    const paperX = 0.75,
+        paperZ = 0.75;
+    let paperCount = 16;
+    let position = [];
+    for (let i = 0; i < paperCount; i++) {
+        let x, y, z;
+        let ok = false;
+        while (!ok) {
+            ok = true;
+            x = Math.random() * 4 - 2;
+            y = Math.random() * 1;
+            z = Math.random() * 4 - 2;
+            for (let j = 0; j < i; j++) {
+                if (
+                    Math.abs(x - position[j].x) < paperX &&
+                    Math.abs(z - position[j].z) < paperZ
+                ) {
+                    ok = false;
+                }
+            }
         }
-      }
+        position.push({ x, y, z });
     }
-    position.push({ x, y, z });
-  }
-  let positionList = [];
-  position.map((item, index) => {
-    positionList.push(`${item.x} ${item.y} ${item.z}`);
-  });
-  return positionList;
+    let positionList = [];
+    position.map((item, index) => {
+        positionList.push(`${item.x} ${item.y} ${item.z}`);
+    });
+    return positionList;
 }
 
 Component({
-  properties: {
-    textureList: {
-      type: Array,
-      value: [],
+    properties: {
+        textureList: {
+            type: Array,
+            value: [],
+        },
     },
-  },
-  data: {
-    loaded: false,
-    scene: undefined,
-    positionList: [
-      "0 0 0",
-      "1 0.25 0.5",
-      "-0.75 1 -0.25",
-      "0.5 0.5 -1",
-      "-1.5 0.4 0.8",
-      "-1.2 0.1 -0.2",
-      "1.8 0.6 0.3",
-      "1.4 1.5 -0.4",
-      "-0.2 1.2 0.3",
-      "-0.4 0.4 -0.8",
-    ],
-    nodeList: [],
-    tracked: false,
-  },
-  ready() {
-    randomPosition();
-    this.setData({ nodeList: this.data.textureList });
-    let positionList = randomPosition();
-    this.setData({ positionList });
-  },
-  methods: {
-    handleReady: function ({ detail }) {
-      this.setData({ scene: detail.value });
-      // detail.value.createImage({
-      //   type: "texture",
-      //   assetId: "paper",
-      //   src: this.data.textureList[0],
-      // });
-
-      console.log(1);
-      console.log(this.data.scene);
+    data: {
+        loaded: false,
+        scene: undefined,
+        positionList: [
+            "0 0 0",
+            "1 0.25 0.5",
+            "-0.75 1 -0.25",
+            "0.5 0.5 -1",
+            "-1.5 0.4 0.8",
+            "-1.2 0.1 -0.2",
+            "1.8 0.6 0.3",
+            "1.4 1.5 -0.4",
+            "-0.2 1.2 0.3",
+            "-0.4 0.4 -0.8",
+        ],
+        nodeList: [],
+        tracked: false,
     },
-    handleAssetsLoaded: function ({ detail }) {
-      let assetList = [];
-      this.data.textureList.map((item, index) => {
-        assetList.push(
-          this.data.scene.assets.loadAsset({
-            type: "texture",
-            assetId: `paper${index}`,
-            src: item,
-          })
-        );
-      });
-      Promise.all(assetList).then(() => {
-        console.log("paper assets loaded");
-        this.setData({ loaded: true });
-      });
+    ready() {
+        randomPosition();
+        this.setData({ nodeList: this.data.textureList });
+        let positionList = randomPosition();
+        this.setData({ positionList });
     },
-    handleTrackerSwitch({ detail }) {
-      console.log("track");
-      this.setData({ tracked: true });
-      this.triggerEvent("artrack", { isShow: true });
+    methods: {
+        handleReady: function ({ detail }) {
+            this.setData({ scene: detail.value });
+        },
+        handleAssetsLoaded: function ({ detail }) {
+            let assetList = [];
+            this.data.textureList.map((item, index) => {
+                assetList.push(
+                    this.data.scene.assets.loadAsset({
+                        type: "texture",
+                        assetId: `paper${index}`,
+                        src: item,
+                    })
+                );
+            });
+            Promise.all(assetList).then(() => {
+                console.log("paper assets loaded");
+                this.setData({ loaded: true });
+            });
+        },
+        handleTrackerSwitch({ detail }) {
+            console.log("track");
+            this.setData({ tracked: true });
+            this.triggerEvent("artrack", { isShow: true });
+        },
+        handleGLTFLoaded: function ({ detail }) {
+            const el = detail.value.target;
+            const gltf = el.getComponent("gltf");
+            const newMat = this.data.scene.assets.getAsset(
+                "texture",
+                `paper${el.id.substring(9)}`
+            );
+            console.log(el.id.substring(9));
+            console.log(newMat);
+            for (const mesh of gltf.getPrimitivesByNodeName("paper")) {
+                mesh.material.setTexture("u_baseColorMap", newMat);
+            }
+        },
+        handleTouchModel: function ({ detail }) {
+            const el = detail.value.target;
+            console.log(el);
+            console.log(this.data.scene.getNodeById(`node${el.id[9]}`));
+        },
+        handleDragModel: function ({ detail }) {
+            const el = detail.value.target;
+            let node = this.data.scene.getNodeById(`node${el.id[9]}`);
+            node.position.x += detail.value.deltaX / 500;
+            node.position.z += detail.value.deltaY / 500;
+        },
     },
-    handleGLTFLoaded: function ({ detail }) {
-      const el = detail.value.target;
-      const gltf = el.getComponent("gltf");
-      const newMat = this.data.scene.assets.getAsset(
-        "texture",
-        `paper${el.id.substring(9)}`
-      );
-      console.log(el.id.substring(9));
-      console.log(newMat);
-      for (const mesh of gltf.getPrimitivesByNodeName("paper")) {
-        mesh.material.setTexture("u_baseColorMap", newMat);
-      }
-    },
-    handleTouchModel: function ({ detail }) {
-      const el = detail.value.target;
-      console.log(el);
-      console.log(this.data.scene.getNodeById(`node${el.id[9]}`));
-    },
-    handleDragModel: function ({ detail }) {
-      const el = detail.value.target;
-      let node = this.data.scene.getNodeById(`node${el.id[9]}`);
-      node.position.x += detail.value.deltaX / 500;
-      node.position.z += detail.value.deltaY / 500;
-    },
-  },
 });
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,153 +5,211 @@ const app = getApp();
 const url = "https://ar-note.hust.online/api/v1";
 
 const color = [
-  "rgb(214, 141, 186)",
-  "rgb(238, 184, 110)",
-  "rgb(120, 196, 150)",
-  "rgb(115, 172, 202)",
-  "rgb(189, 152, 221)",
-  "rgb(255, 255, 255)",
+    "rgb(214, 141, 186)",
+    "rgb(238, 184, 110)",
+    "rgb(120, 196, 150)",
+    "rgb(115, 172, 202)",
+    "rgb(189, 152, 221)",
+    "rgb(255, 255, 255)",
 ];
 
-Page({
-  data: {
-    width: 300,
-    height: 300,
-    renderWidth: 300,
-    renderHeight: 300,
-    postList: [],
-    textureList: [],
-    isShowAR: false,
-    isShowSendBtn: false,
-    position: "东九教学楼",
-  },
-  onLoad(e) {
-    if (e.position && e.position !== "undefined") {
-      this.setData({ position: e.position });
+function compareVersion(v1, v2) {
+    v1 = v1.split(".");
+    v2 = v2.split(".");
+    const len = Math.max(v1.length, v2.length);
+
+    while (v1.length < len) {
+        v1.push("0");
+    }
+    while (v2.length < len) {
+        v2.push("0");
     }
 
-    const info = wx.getSystemInfoSync();
-    const width = info.windowWidth;
-    const height = info.windowHeight;
-    const dpi = info.pixelRatio;
-    this.setData({
-      width,
-      height,
-      renderWidth: width * dpi,
-      renderHeight: height * dpi,
-    });
-  },
-  onReady() {
-    let that = this;
-    let promiseList = [];
-    this.methods.getPostList(that).then((res) => {
-      res.map((item, index) => {
-        promiseList.push(this.methods.createText(item, index));
-      });
-      Promise.all(promiseList).then((res) => {
-        console.log(res);
-        this.setData({ textureList: res });
-        this.setData({ isShowAR: true });
-      });
-    });
+    for (let i = 0; i < len; i++) {
+        const num1 = parseInt(v1[i]);
+        const num2 = parseInt(v2[i]);
 
-    setInterval(() => {
-      const arChild = this.selectComponent("#main-frame");
-      if (arChild.data.tracked) {
-        this.setData({ isShowSendBtn: true });
-      }
-    }, 500);
-  },
-  onARTrack(e) {
-    console.log(e.detail);
-    console.log(1);
-    this.setData({ isShowSendBtn: true });
-  },
-  sendHandler(e) {
-    wx.navigateTo({
-      url: "../write/write",
-    });
-  },
-  methods: {
-    getPostList: function (that) {
-      console.log(that.data);
-      let instance = new Promise((resolve, reject) => {
-        wx.request({
-          url: `${url}/post?location_name=${that.data.position}`,
-          method: "GET",
-          success: (res) => {
-            console.log(res);
-            that.setData({ postList: res.data.data.post_list });
-            resolve(res.data.data.post_list);
-          },
+        if (num1 > num2) {
+            return 1;
+        } else if (num1 < num2) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+Page({
+    data: {
+        width: 300,
+        height: 300,
+        renderWidth: 300,
+        renderHeight: 300,
+        postList: [],
+        textureList: [],
+        isShowAR: false,
+        isShowSendBtn: false,
+        position: "东九教学楼",
+    },
+    onLoad(e) {
+        if (e.position && e.position !== "undefined") {
+            this.setData({ position: e.position });
+        }
+
+        const info = wx.getSystemInfoSync();
+        const width = info.windowWidth;
+        const height = info.windowHeight;
+        const dpi = info.pixelRatio;
+        this.setData({
+            width,
+            height,
+            renderWidth: width * dpi,
+            renderHeight: height * dpi,
         });
 
-        // let postList = [];
-        // for (let i = 0; i < 10; i++)
-        //   postList.push({
-        //     content: "HelloWorld 这是什么这是什么",
-        //     user_name: "ligen131",
-        //   });
-        // resolve(postList);
-      });
-      return instance;
-    },
-    createText: function (item, index) {
-      let instance = new Promise((resolve, reject) => {
-        wx.createSelectorQuery()
-          .select("#canvas" + index)
-          .fields({ node: true, size: true })
-          .exec((res) => {
-            console.log(res);
-            const canvas = res[0].node;
-            const ctx = canvas.getContext("2d");
-            const dpr = wx.getWindowInfo().pixelRatio;
-            let width = 150;
-            let height = 150;
-            canvas.width = width * dpr * 2;
-            canvas.height = height * dpr * 2;
-            ctx.scale(dpr * 2, dpr * 2);
-            ctx.font = "8px";
-            ctx.fillStyle = color[Math.floor(Math.random() * 6)];
-            ctx.fillRect(0, 0, 150, 150);
+        const version = wx.getAppBaseInfo().SDKVersion;
 
-            ctx.fillStyle = "rgb(0,0,0)";
-            let maxWidth = 100;
-            let line = 0;
-            let str = "";
-            let text = item.content;
-            for (let i = 0; i < text.length; i++) {
-              str += text.charAt(i);
-              if (ctx.measureText(str).width > maxWidth) {
-                ctx.fillText(
-                  str.substring(0, str.length - 1),
-                  42,
-                  50 + line * 10
-                );
-                str = "";
-                i--;
-                line++;
-              }
-            }
-            ctx.fillText(str, 42, 50 + line * 10);
-            ctx.font = "6px";
-            let userWidth = ctx.measureText(item.user_name).width;
-            ctx.fillText(
-              item.user_name,
-              42 + maxWidth - userWidth,
-              50 + line * 10 + 30
-            );
-
-            wx.canvasToTempFilePath({
-              canvas,
-              success: (res) => {
-                const tempFilePath = res.tempFilePath;
-                resolve(tempFilePath);
-              },
+        if (compareVersion(version, "2.27.1") >= 0) {
+            console.log("当前版本大于2.27.1");
+        } else {
+            console.log("当前版本过低");
+            wx.showModal({
+                title: "提示",
+                content:
+                    "当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。",
             });
-          });
-      });
-      return instance;
+        }
     },
-  },
+    onReady() {
+        let that = this;
+        let promiseList = [];
+        wx.getStorage({
+            key: "used",
+            fail: (res) => {
+                console.log(res);
+                wx.setStorage({
+                    key: "used",
+                    data: 1,
+                });
+                promiseList.push(
+                    that.methods.createText(
+                        {
+                            content: "纸条可以被随意拖动哦！",
+                            user_name: "",
+                        },
+                        16
+                    )
+                );
+            },
+        });
+        this.methods.getPostList(that).then((res) => {
+            res.map((item, index) => {
+                promiseList.push(this.methods.createText(item, index));
+            });
+            Promise.all(promiseList).then((res) => {
+                console.log(res);
+                this.setData({ textureList: res });
+                this.setData({ isShowAR: true });
+            });
+        });
+
+        setInterval(() => {
+            const arChild = this.selectComponent("#main-frame");
+            if (arChild.data.tracked) {
+                this.setData({ isShowSendBtn: true });
+            }
+        }, 500);
+    },
+    onARTrack(e) {
+        console.log(e.detail);
+        console.log(1);
+        this.setData({ isShowSendBtn: true });
+    },
+    sendHandler(e) {
+        wx.navigateTo({
+            url: "../write/write",
+        });
+    },
+    methods: {
+        getPostList: function (that) {
+            console.log(that.data);
+            let instance = new Promise((resolve, reject) => {
+                wx.request({
+                    url: `${url}/post?location_name=${that.data.position}&limit=15`,
+                    method: "GET",
+                    success: (res) => {
+                        console.log(res);
+                        that.setData({ postList: res.data.data.post_list });
+                        resolve(res.data.data.post_list);
+                    },
+                });
+
+                // let postList = [];
+                // for (let i = 0; i < 10; i++)
+                //   postList.push({
+                //     content: "HelloWorld 这是什么这是什么",
+                //     user_name: "ligen131",
+                //   });
+                // resolve(postList);
+            });
+            return instance;
+        },
+        createText: function (item, index) {
+            let instance = new Promise((resolve, reject) => {
+                wx.createSelectorQuery()
+                    .select("#canvas" + index)
+                    .fields({ node: true, size: true })
+                    .exec((res) => {
+                        console.log(res);
+                        const canvas = res[0].node;
+                        const ctx = canvas.getContext("2d");
+                        const dpr = wx.getWindowInfo().pixelRatio;
+                        let width = 150;
+                        let height = 150;
+                        canvas.width = width * dpr * 2;
+                        canvas.height = height * dpr * 2;
+                        ctx.scale(dpr * 2, dpr * 2);
+                        ctx.font = "8px";
+                        ctx.fillStyle = color[Math.floor(Math.random() * 6)];
+                        ctx.fillRect(0, 0, 150, 150);
+
+                        ctx.fillStyle = "rgb(0,0,0)";
+                        let maxWidth = 100;
+                        let line = 0;
+                        let str = "";
+                        let text = item.content;
+                        for (let i = 0; i < text.length; i++) {
+                            str += text.charAt(i);
+                            if (ctx.measureText(str).width > maxWidth) {
+                                ctx.fillText(
+                                    str.substring(0, str.length - 1),
+                                    42,
+                                    50 + line * 10
+                                );
+                                str = "";
+                                i--;
+                                line++;
+                            }
+                        }
+                        ctx.fillText(str, 42, 50 + line * 10);
+                        ctx.font = "6px";
+                        let userWidth = ctx.measureText(item.user_name).width;
+                        ctx.fillText(
+                            item.user_name,
+                            42 + maxWidth - userWidth,
+                            50 + line * 10 + 30
+                        );
+
+                        wx.canvasToTempFilePath({
+                            canvas,
+                            success: (res) => {
+                                const tempFilePath = res.tempFilePath;
+                                resolve(tempFilePath);
+                            },
+                        });
+                    });
+            });
+            return instance;
+        },
+    },
 });
